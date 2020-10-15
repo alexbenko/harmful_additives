9eb7f99abef4d2b51261620bf2129d236a27a396
Alex Benko <benkoalex521@gmail.com>
2020-10-15 15:38:46 -0700

    Project setup with typescript

---
+++ components/Layout.tsx
@@ -0,0 +1,42 @@
+//this will be the layout for the entire app proabably
+//will most likely start out as a single page app
+
+import React, { ReactNode } from 'react'
+//import Link from 'next/link'
+import Head from 'next/head'
+
+import { FaRegCopyright } from 'react-icons/fa';
+
+type Props = {
+  children?: ReactNode
+  title?: string
+  description?: string
+}
+
+const Layout = ({ children, title = 'Food?', description='Does your food contain known carcinogens ?' }: Props) =>{
+  return(
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="og:keywords" content="Cancer, my food,is bad for me,cause cancer"/>
+        <meta name="og:description" content={description}/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+
+      <header>
+        Header
+      </header>
+
+      {children}
+
+      <footer>
+        <hr/>
+        <FaRegCopyright />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+//import Layout from '../components/Layout'
+
+const IndexPage = () => {
+  return(
+    <div className="index">
+      <h1>Hello 👋</h1>
+    </div>
+  )
+}
+
+export default IndexPage
