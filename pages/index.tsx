61903d919a7e8d942da56e70d2707f931233041e
Alex Benko <benkoalex521@gmail.com>
2020-10-16 14:01:10 -0700

    Functions to handle user events

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import axios from 'axios';
+import { useState } from 'react'
 const generateSearchQuery = async (word:string):Promise<string> =>{
   let output = ''
   let term = word.split(' ');
 
   term.map((word)=> output = output + '+' + word)
-  console.log(term)
   return Promise.resolve(output)
 };
 
@@ -11,13 +12,35 @@ const goToSearch = async () =>{
   let query = await generateSearchQuery('agave nectar')
 
   window.open(`https://google.com/search?q=${query}+bad+for+you`)
-}
+};
+
 
 const IndexPage = () => {
+  const [search,setSearch] = useState('');
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    axios.post('/api/analyze',{search:search})
+
+  }
+
   return(
     <div className="index">
+      <div className="welcome-paragraph">
+        <p>
+
+        </p>
+
+      </div>
+
+      <div className="forms">
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <input type="submit" style={{display: "none"}} />
+          <input type="text" value={search} style={{ padding: '10px'}} onChange={(e)=>setSearch(e.target.value)}/>
+        </form>
+
+      </div>
       <h1>Hello 👋</h1>
-      <button onClick={()=>goToSearch()}></button>
+
     </div>
   )
 }
