9043636ef69c8d6045196d227b9396eba2d57710
Alex Benko <benkoalex521@gmail.com>
2020-10-18 10:38:47 -0700

    Component that will display any search terms entered by the user with a way to remove them from the query

---
+++ components/Term.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type TermCon = {
+  remove: Function
+  term: string
+}
+
+const Term = ({remove,term}:TermCon) =>{
+  return(
+    <div className={`${name}-container`} style={{display:'flex',flexDirection:'row'}}>
+      <p>{term}</p>
+      <span style={{cursor:'pointer'}} onClick={()=>remove(term)}>&#10006;</span>
+    </div>
+  )
+};
+
+export default Term
