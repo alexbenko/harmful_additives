83b0524545ef547a3139225b4d97477f60ffc45b
Alex Benko <benkoalex521@gmail.com>
2020-10-18 10:46:17 -0700

    Still need to work on retireving matches to send back

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,23 +1,32 @@
 import dictionary from '../../result'
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const analyze = async(terms:Array<string>):Promise<Array<string>> =>{
+const analyze = () =>{
   //O: object containing all detected harmful ingredients
   //I: array containing strings of each ingredient to search for
+  let detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
 
-  terms.map((term)=>{
-
+  terms.map((term:string):void=>{
+    if(term in dictionary["misc"]) {
+      console.log(dictionary["misc"][term])
+      detected.misc.push(dictionary["misc"][term])
+    } else if(term in dictionary["colors"]){
+      console.log(dictionary["colors"][term]);
+    } else if(term in dictionary["sweeteners"]){
+      console.log(dictionary["sweeteners"][term])
+    }
   })
 
 
-  return Promise.resolve([''])
+  //return Promise.resolve({})
 }
 
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    console.log(req.body.search)
-    res.status(200).json('Success')
+    console.log(typeof req.query)
+    analyze(req.query['copy[]'])
+    res.status(200).json(req.query['copy[]'][0])
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
