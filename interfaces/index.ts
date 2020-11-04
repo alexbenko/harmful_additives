bc460b70e36ae2b2bd75954b07360c5d63366a12
Alex Benko <benkoalex521@gmail.com>
2020-11-04 10:03:02 -0800

    Interface for an array of objects

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,3 +3,10 @@ export type Dictionary = {
   colors: object
   sweeteners: object
 }
+
+export interface Detected {
+  misc: {title:string,why:string}[]
+  colors:{title:string,why:string}[]
+  sweeteners:{title:string,why:string}[]
+  unknown:string[]
+}
