b75ede7a3eba7db9d70429205d9c72bec8cfaa4d
Alex Benko <benkoalex521@gmail.com>
2020-11-04 10:04:03 -0800

    Successfully retrieves data from JSON based on user input

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,32 +1,37 @@
-import dictionary from '../../result'
+import dictionary from '../../data/result'
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Detected } from '../../interfaces';
 
-const analyze = () =>{
+const analyze = async(terms) =>{
   //O: object containing all detected harmful ingredients
   //I: array containing strings of each ingredient to search for
-  let detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
+  let detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
 
   terms.map((term:string):void=>{
     if(term in dictionary["misc"]) {
       console.log(dictionary["misc"][term])
-      detected.misc.push(dictionary["misc"][term])
+      detected.misc.push({title:term,why: dictionary["misc"][term]})
     } else if(term in dictionary["colors"]){
-      console.log(dictionary["colors"][term]);
+      console.log(dictionary["colors"][term])
+      detected.colors.push({title:term,why: dictionary["colors"][term]})
     } else if(term in dictionary["sweeteners"]){
       console.log(dictionary["sweeteners"][term])
+      detected.sweeteners.push({title:term,why: dictionary["sweeteners"][term]})
+    } else {
+      detected.unknown.push(term)
     }
   })
 
 
-  //return Promise.resolve({})
+  return detected
 }
 
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async(req: NextApiRequest, res: NextApiResponse) => {
   try {
-    console.log(typeof req.query)
-    analyze(req.query['copy[]'])
-    res.status(200).json(req.query['copy[]'][0])
+    console.log(typeof req.query['copy[]'])
+    let results = await analyze(req.query['copy[]'])
+    res.status(200).json(results)
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
