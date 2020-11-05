d4814b5b179e1338095117b115c15b9b14bab5cd
Alex Benko <benkoalex521@gmail.com>
2020-11-04 19:15:51 -0800

    When clicking on a title, it opens that term in a new window

--- components/Individual.tsx
+++ components/Individual.tsx
@@ -1,8 +1,21 @@
+const generateSearchQuery = async (word:string):Promise<string> =>{
+  let output = ''
+  let term = word.split(' ');
+
+  term.map((word)=> output = output + '+' + word)
+  return Promise.resolve(output)
+};
+
+const goToSearch = async (search:string = 'agave nectar') =>{
+  let query = await generateSearchQuery(search)
+
+  window.open(`https://google.com/search?q=${query}+bad+for+you`)
+};
 
 const Individual = ({title, description})=>{
   return(
     <div>
-      <h2>{title}</h2>
+      <h2 onClick={()=> goToSearch(title)}>{title}</h2>
       <p>{description}</p>
     </div>
 
