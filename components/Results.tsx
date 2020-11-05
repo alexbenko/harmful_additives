8a73e0cc846c30b0c4dd98139e875f67c26d1b0c
Alex Benko <benkoalex521@gmail.com>
2020-11-04 19:15:25 -0800

    Added more interfaces

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,12 +1,13 @@
 import { Detected } from '../interfaces';
 import Individual from '../components/Individual';
+import { Result } from '../interfaces'
 
-const Results = (detected:Detected)=>{
+const Results = ({detected} )=>{
   return(
     <div className="results-container">
 
-      <div className="misc-contianer">
-        {detected.misc.map((item,i)=><Individual title={item.title} description={item.why}/>)}
+      <div className="misc-contianer" style={{borderStyle: 'solid'}}>
+        {detected.misc.map((item: Result, i:number )=><Individual key={i} title={item.title} description={item.why}/>)}
       </div>
     </div>
   )
