c4d85bac8b12e1f9caf66976f0aa6f3b696c3a35
Alex Benko <benkoalex521@gmail.com>
2020-11-04 19:15:04 -0800

    Added interface for each individual result object

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,12 +1,11 @@
-export type Dictionary = {
-  cancer: object
-  colors: object
-  sweeteners: object
-}
-
 export interface Detected {
   misc: {title:string,why:string}[]
   colors:{title:string,why:string}[]
   sweeteners:{title:string,why:string}[]
   unknown:string[]
+}
+
+export interface Result {
+  title: string
+  why: string
 }
