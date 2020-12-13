3ba453745bbae4cb674c9c2e5dc0c52fae416dee
Alex Benko <benkoalex521@gmail.com>
2020-12-12 23:03:46 -0800

    Removed interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,11 +1,6 @@
-export interface Detected {
+export interface Detected{
   misc: {title:string,why:string}[]
   colors:{title:string,why:string}[]
   sweeteners:{title:string,why:string}[]
   unknown:string[]
-}
-
-export interface Result {
-  title: string
-  why: string
 }
