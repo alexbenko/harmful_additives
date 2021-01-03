149e402a647007a0a35f2f181904e9c76221e94e
Alex Benko <benkoalex521@gmail.com>
2021-01-03 01:06:51 -0800

    Function works with just one search now

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,32 +1,49 @@
 import dictionary from '../../data/result'
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Detected } from '../../interfaces';
+import { type } from 'os';
 
 const analyze = async(terms):Promise<any> =>{
+  console.log(typeof terms)
   //O: object containing all detected harmful ingredients
   //I: array containing strings of each ingredient to search for
   let detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
-  //this sanitzes each search term to have no white space and all lower case
-  let sanitzed = await Promise.all(
-    terms.map((term:string)=>{
-      return term.replace(/ /g,'').toLowerCase()
-    })
-  );
-
-  sanitzed.map((term:string,i):void=>{
-    if(term in dictionary["misc"]) {
-      detected.misc.push({title:terms[i],why: dictionary["misc"][term]})
-    } else if(term in dictionary["colors"]){
-      detected.colors.push({title:terms[i],why: dictionary["colors"][term]})
-    } else if(term in dictionary["sweeteners"]){
-      detected.sweeteners.push({title:terms[i],why: dictionary["sweeteners"][term]})
+  if(typeof terms === 'string'){
+    let cleaned = terms.replace(/ /g,'').toLowerCase()
+    if(terms in dictionary["misc"]) {
+      detected.misc.push({title:terms,why: dictionary["misc"][terms]})
+    } else if(terms in dictionary["colors"]){
+      detected.colors.push({title:terms,why: dictionary["colors"][terms]})
+    } else if(terms in dictionary["sweeteners"]){
+      detected.sweeteners.push({title:terms,why: dictionary["sweeteners"][terms]})
     } else {
-      detected.unknown.push(term)
+      detected.unknown.push(terms)
     }
-  })
+    return detected
+  } else {
+    //this sanitzes each search term to have no white space and all lower case
+    let sanitzed = await Promise.all(
+      terms.map((term:string)=>{
+        return term.replace(/ /g,'').toLowerCase()
+      })
+    );
 
+    sanitzed.map((term:string,i):void=>{
+      if(term in dictionary["misc"]) {
+        detected.misc.push({title:terms[i],why: dictionary["misc"][term]})
+      } else if(term in dictionary["colors"]){
+        detected.colors.push({title:terms[i],why: dictionary["colors"][term]})
+      } else if(term in dictionary["sweeteners"]){
+        detected.sweeteners.push({title:terms[i],why: dictionary["sweeteners"][term]})
+      } else {
+        detected.unknown.push(term)
+      }
+    })
 
-  return detected
+    return detected
+
+  }
+
 }
 
 
@@ -42,7 +59,7 @@ const handler = async(req: NextApiRequest, res: NextApiResponse) => {
     console.log(response)
     res.status(200).json(response)
   } catch (err) {
-    console.log(err)
+    console.error(err)
     res.status(400).json({ statusCode: 400, message: err.message })
   }
 }
