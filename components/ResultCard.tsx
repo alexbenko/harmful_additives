5072cb808714f42c936494224e6de1b6c7b471bc
Alex Benko <benkoalex521@gmail.com>
2021-02-15 10:22:57 -0800

    Conditiotonal Render

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -1,18 +1,22 @@
 import Individual from '../components/Individual';
 
 const ResultCard = ({ header , subtitle , results})=>{
-    return(
-      <div className={`${header}-card`} style={{display:'flext',flexDirection:'column'}}>
-        <div className="card-header">
-          <h2>{header}</h2>
-          <p>{subtitle}</p>
-        </div>
+  return (
+    results.length !== 0 &&
 
-        <div className="results-container">
-          {results.map((result, i:number )=><Individual key={i} title={result.title} description={result.why}/>)}
-        </div>
+    <div className={`${header}-card`} style={{display:'flext',flexDirection:'column'}}>
+      <div className="card-header">
+        <h2>{header}</h2>
+        <p>{subtitle}</p>
       </div>
-    )
+
+      <div className="results-container">
+        {results.map((result, i:number )=><Individual key={i} title={result.title} description={result.why}/>)}
+      </div>
+    </div>
+
+  )
+
 }
 
 export default ResultCard
