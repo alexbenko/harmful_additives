df9ce943e18e7d7a0e6e340146cdb85744b57ff3
Alex Benko <benkoalex521@gmail.com>
2021-02-16 11:49:46 -0800

    Cleanup

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,59 +1,19 @@
-import dictionary from '../../data/result'
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Detected } from '../../interfaces';
-
-const analyze = async(terms):Promise<object> =>{
-  //O: object containing all detected harmful ingredients
-  //I: array containing strings of each ingredient to search for
-  let detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
-  if(typeof terms === 'string'){
-    let cleaned = terms.replace(/ /g,'').toLowerCase()
-    if(cleaned in dictionary["misc"]) {
-      detected.misc.push({title:terms,why: dictionary["misc"][terms]})
-    } else if(cleaned in dictionary["colors"]){
-      detected.colors.push({title:terms,why: dictionary["colors"][terms]})
-    } else if(cleaned in dictionary["sweeteners"]){
-      detected.sweeteners.push({title:terms,why: dictionary["sweeteners"][terms]})
-    } else {
-      detected.unknown.push(terms)
-    }
-    return detected
-  } else {
-    //this sanitzes each search term to have no white space and all lower case
-    let sanitzed = await Promise.all(
-      terms.map((term:string)=>{
-        return term.replace(/ /g,'').toLowerCase()
-      })
-    );
-
-    sanitzed.map((term:string,i):void=>{
-      if(term in dictionary["misc"]) {
-        detected.misc.push({title:terms[i],why: dictionary["misc"][term]})
-      } else if(term in dictionary["colors"]){
-        detected.colors.push({title:terms[i],why: dictionary["colors"][term]})
-      } else if(term in dictionary["sweeteners"]){
-        detected.sweeteners.push({title:terms[i],why: dictionary["sweeteners"][term]})
-      } else {
-        detected.unknown.push(term)
-      }
-    })
-
-    return detected
-
-  }
-
-}
-
+import search from '../../lib/db';
 
 const handler = async(req: NextApiRequest, res: NextApiResponse) => {
-  const parsed = JSON.parse(req.body)
+  const parsed = JSON.parse(req.body);
+
+  const response = {
+    userSearches: parsed,
+    results:{}
+  };
+
   try {
-    const response = {
-      userSearches: parsed,
-      results:{}
-    };
-    response.results = await analyze(parsed)
-    console.log('Results: ',response.results)
+    const data = await search(parsed);
+    console.log('Data: ',data)
+    response.results = data;
+    console.log('Response: ',response)
     res.status(200).send(response)
   } catch (err) {
     console.error(err)
