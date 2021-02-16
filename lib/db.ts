88e9978c575e19f6a909e4b9ff1aac416a4d70b8
Alex Benko <benkoalex521@gmail.com>
2021-02-16 11:48:47 -0800

    Custom mongodb search function that returns the same data structure

---
+++ lib/db.ts
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alexbenko:phelps521@cluster0.nvy3b.mongodb.net/foodfinder?retryWrites=true&w=majority";
+import { Detected } from '../interfaces'
+
+const search = async(searches:[string]):Promise<any>=>{
+  console.log("Searching...")
+  const detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
+  const client = await new MongoClient.connect(uri, { useNewUrlParser: true,useUnifiedTopology:true });
+
+  try{
+      const db = client.db("foodfinder").collection("matches");
+      const matches = await db.find( { $text: { $search: searches.join().replace(","," ")} } ).toArray();
+
+      matches.map((term,i):void=>{
+        if(term.category === "misc") {
+          detected.misc.push({title:term.name,why: term.why})
+        } else if(term === "colors"){
+          detected.colors.push({title:term.name,why: term.why})
+        } else if(term.category === "sweeteners"){
+          detected.sweeteners.push({title:term.name,why: term.why})
+        } else if(!(searches[i] in matches[i])){ //if a term from the users search is not in the matches
+          detected.unknown.push(term.name)
+        }
+      })
+      console.log(detected)
+
+      return detected
+
+  }catch(err){
+    throw err;
+  } finally{
+    client.close()
+  }
+};
+
+export default search;
