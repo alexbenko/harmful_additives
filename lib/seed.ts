65d5f3ba0795ad058fe61925f6f8af5f49f23ca4
Alex Benko <benkoalex521@gmail.com>
2021-02-16 10:15:27 -0800

    seed script

---
+++ lib/seed.ts
@@ -0,0 +1,11 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alexbenko:phelps521@cluster0.nvy3b.mongodb.net/foodfinder?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology:true });
+
+const data = require("../data/result.json");
+client.connect(async err => {
+  const collection = await client.db("foodfinder").collection("matches").remove({}).insertMany(data); //clears collection and inserts data
+  // perform actions on the collection object
+  console.log(collection.insertedCount)
+  client.close();
+});
