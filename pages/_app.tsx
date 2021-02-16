b206dc29d74d75efceb481496b715b9ae14abf7b
Alex Benko <benkoalex521@gmail.com>
2021-02-15 21:35:48 -0800

    Support for MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import Layout from '../components/Layout'
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  },[])
   return <Layout> <Component {...pageProps} /> </Layout>
 }
 
