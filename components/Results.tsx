2b0e2b80cbe539e192f0c58edf21dc0f64ddc3c8
Alex Benko <benkoalex521@gmail.com>
2021-02-19 15:17:28 -0800

    Fixed conditional rendering

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,27 +1,39 @@
-import ResultCard from './ResultCard'
+import ResultCard from './ResultCard';
+import { Detected } from '../interfaces';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const Results = ({ detected })=>{
-  return(
-    <div className="results-container">
-      <ResultCard
-        header={'Artificial Sweeteners'}
-        subtitle={''}
-        results={detected.sweeteners}
-      />
+  console.log(detected.sweeteners)
+  if(!(detected.sweeteners.length > 0) && !(detected.colors.length > 0) && !(detected.misc.length > 0)){
+    return (
+      <div>
+          No Matches
+      </div>
+    )
+  } else {
+    return(
+      <div className="results-container">
+        <ResultCard
+          header={'Artificial Sweeteners'}
+          subtitle={''}
+          results={detected.sweeteners}
+        />
 
-      <ResultCard
-        header={'Artificial Coloring'}
-        subtitle={''}
-        results={detected.colors}
-      />
+        <ResultCard
+          header={'Artificial Coloring'}
+          subtitle={''}
+          results={detected.colors}
+        />
 
-      <ResultCard
-        header={'Miscellaneous'}
-        subtitle={'A list of other harmful ingredients found in your food'}
-        results={detected.misc}
-      />
-    </div>
-  )
+        <ResultCard
+          header={'Miscellaneous'}
+          subtitle={'A list of other harmful ingredients found in your food'}
+          results={detected.misc}
+        />
+      </div>
+    )
+  }
 }
 
 export default Results;
