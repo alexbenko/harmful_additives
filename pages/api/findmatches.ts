044ff453f289d0d28f0326ae1aebe1d0cf807669
Alex Benko <benkoalex521@gmail.com>
2021-02-22 08:34:32 -0800

    Fixed import

--- pages/api/findmatches.ts
+++ pages/api/findmatches.ts
@@ -1,8 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import search from '../../lib/db';
-import { Detected } from '../interfaces'
+import { Detected } from '../../interfaces'
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://alexbenko:phelps521@cluster0.nvy3b.mongodb.net/foodfinder?retryWrites=true&w=majority";
+
+const handler = async(req: NextApiRequest, res: NextApiResponse) => {
+
+  const parsed = JSON.parse(req.body);
+
+  const response = {
+    success: false,
+    noMatches: false,
+    userSearches: parsed,
+    results:{}
+  };
+
+  try {
+    const data = await search(parsed);
+    response.results = data;
+    response.success = true;
+    res.status(200).send(response)
+  } catch (err) {
+    if(err === "no matches"){
+      response.noMatches = true;
+      response.success = true;
+      response.results = {misc:[],colors:[],sweeteners:[],unknown:[]}
+      res.status(200).send(response);
+      return
+    }
+    response.success = false;
+    console.error('[API ERROR]',err)
+    res.status(400).json({ statusCode: 400, message: err.message })
+  }
+}
+
+export default handler
 
 async function search (searches:[string]):Promise<Detected>{
   const detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
@@ -47,36 +78,3 @@ async function search (searches:[string]):Promise<Detected>{
     client.close()
   }
 };
-
-
-const handler = async(req: NextApiRequest, res: NextApiResponse) => {
-
-  const parsed = JSON.parse(req.body);
-
-  const response = {
-    success: false,
-    noMatches: false,
-    userSearches: parsed,
-    results:{}
-  };
-
-  try {
-    const data = await search(parsed);
-    response.results = data;
-    response.success = true;
-    res.status(200).send(response)
-  } catch (err) {
-    if(err === "no matches"){
-      response.noMatches = true;
-      response.success = true;
-      response.results = {misc:[],colors:[],sweeteners:[],unknown:[]}
-      res.status(200).send(response);
-      return
-    }
-    response.success = false;
-    console.error('[API ERROR]',err)
-    res.status(400).json({ statusCode: 400, message: err.message })
-  }
-}
-
-export default handler
