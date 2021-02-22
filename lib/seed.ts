b8ad2e0cbdb1764fde068264e9d3fab655f079e5
Alex Benko <benkoalex521@gmail.com>
2021-02-21 21:14:35 -0800

    Bux fixes

--- lib/seed.ts
+++ lib/seed.ts
@@ -1,11 +1,12 @@
-export {};
+export {}
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://alexbenko:phelps521@cluster0.nvy3b.mongodb.net/foodfinder?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology:true });
 
 const data = require("../data/result.json");
 client.connect(async err => {
-  const collection = await client.db("foodfinder").collection("matches").remove({}).insertMany(data); //clears collection and inserts data
+  const collection = await client.db("foodfinder").collection("matches").createIndex( { name: "text", description: "text" } )//.insertMany(data); //clears collection and inserts data
   // perform actions on the collection object
   console.log(collection.insertedCount)
   client.close();
