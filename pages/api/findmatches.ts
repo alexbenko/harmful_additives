d10c46269a085a32b8a21031f200e7fdd8b10155
Alex Benko <benkoalex521@gmail.com>
2021-02-22 08:34:17 -0800

    Removed unnecessary export and import

--- pages/api/findmatches.ts
+++ pages/api/findmatches.ts
@@ -1,9 +1,10 @@
-export {}
+import { NextApiRequest, NextApiResponse } from 'next';
+import search from '../../lib/db';
+import { Detected } from '../interfaces'
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://alexbenko:phelps521@cluster0.nvy3b.mongodb.net/foodfinder?retryWrites=true&w=majority";
-import { Detected } from '../interfaces'
 
-const search = async(searches:[string]):Promise<any>=>{
+async function search (searches:[string]):Promise<Detected>{
   const detected:Detected = {misc:[],colors:[],sweeteners:[],unknown:[]};
   const client = await new MongoClient.connect(uri, { useNewUrlParser: true,useUnifiedTopology:true });
 
@@ -47,4 +48,35 @@ const search = async(searches:[string]):Promise<any>=>{
   }
 };
 
-export default search;
+
+const handler = async(req: NextApiRequest, res: NextApiResponse) => {
+
+  const parsed = JSON.parse(req.body);
+
+  const response = {
+    success: false,
+    noMatches: false,
+    userSearches: parsed,
+    results:{}
+  };
+
+  try {
+    const data = await search(parsed);
+    response.results = data;
+    response.success = true;
+    res.status(200).send(response)
+  } catch (err) {
+    if(err === "no matches"){
+      response.noMatches = true;
+      response.success = true;
+      response.results = {misc:[],colors:[],sweeteners:[],unknown:[]}
+      res.status(200).send(response);
+      return
+    }
+    response.success = false;
+    console.error('[API ERROR]',err)
+    res.status(400).json({ statusCode: 400, message: err.message })
+  }
+}
+
+export default handler
