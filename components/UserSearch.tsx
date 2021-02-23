bd0353fd45d080ef1734d1ec014850f2fd52c561
Alex Benko <benkoalex521@gmail.com>
2021-02-23 11:04:46 -0800

    User search shows whole word and adjusts automatically and added tooltip

--- components/UserSearch.tsx
+++ components/UserSearch.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
+import ClearIcon from '@material-ui/icons/Clear';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+
+
 type TermCon = {
   remove: Function
   term: string
 }
 
-const useStyles = makeStyles({
+/*const useStyles = makeStyles({
   root: {
     maxWidth: 400,
-    flex:'1 0 21%',
+    //flex:'1 0 21%',
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    //justifyContent: 'space-between',
   },
   title: {
     fontSize: 14,
@@ -27,9 +29,48 @@ const useStyles = makeStyles({
     overflow:'hidden'
   }
 });
+*/
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      //flexGrow: 1,
+      overflow: 'hidden',
+      padding: theme.spacing(0, 3),
+      maxWidth:300
+    },
+    icon:{
+      position:'absolute',
+      top:0,
+      right:0,
+      padding:'0'
+    },
+    paper: {
+      position:'relative',
+      maxWidth: 400,
+      margin: `${theme.spacing(1)}px auto`,
+      padding: '10px'//theme.spacing(2),
+    },
+  }),
+);
 
 const UserSearch = ({remove,term}:TermCon) =>{
   const styles = useStyles();
+  return(
+    <div className={styles.root}>
+      <Paper className={styles.paper}>
+        <Tooltip title="Delete From Search" arrow>
+          <IconButton onClick={()=>remove(term)} aria-label={`delete-${term}`} className={styles.icon}>
+            <ClearIcon fontSize="small"/>
+          </IconButton>
+        </Tooltip>
+        <Grid item >
+          <Typography style={{padding:'10px'}}>{term}</Typography>
+        </Grid>
+      </Paper>
+
+    </div>
+  )
+  /*
   return(
     <Grid item xs={6} lg={3}>
       <Card className={styles.root}>
@@ -46,6 +87,7 @@ const UserSearch = ({remove,term}:TermCon) =>{
       </Card>
     </Grid>
   )
+  */
 };
 
 export default UserSearch;
