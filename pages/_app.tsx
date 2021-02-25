4c3602d95c0f02962999f77211ca92c3142ffbd6
Alex Benko <benkoalex521@gmail.com>
2021-02-25 00:41:22 -0800

    Meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="og:keywords" content="Cancer, my food,is bad for me,cause cancer,what is in,my food,what is in my food,what is"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="A tool used to see if your food contains any harmful ingredients. Simply upload a picture of the ingredient list/manually type out the suspicious ingredients and hit search."/>
       </Head>
       <Layout>
         <Component {...pageProps} />
