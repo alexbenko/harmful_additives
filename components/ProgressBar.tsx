003dea66af59a32939438eb78c478fa50f7f53d4
Alex Benko <benkoalex521@gmail.com>
2021-02-25 17:25:08 -0800

    Layout imrpovements

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,19 +1,31 @@
 import LinearProgress, { LinearProgressProps } from '@material-ui/core/LinearProgress';
 import Box from '@material-ui/core/Box';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      color:'#FFFFFF',
+      flexDirection:'column'
+    },
+  }),
+);
 
 const ProgressBar = (props: LinearProgressProps & { value: number, action: string })=>{
+  const styles = useStyles();
   return(
-    <Box display="flex" alignItems="center">
-      <Box width="100%" mr={1}>
-        <LinearProgress variant="determinate" {...props} />
+    <div>
+      <Box display="flex" alignItems="center" className={styles.root}>
+        <Box width="49%" mr={1}>
+          <LinearProgress variant="determinate" {...props} />
+        </Box>
+        <Box minWidth={35}>
+          <Typography variant="body2" >{`${Math.round(props.value,)}%`}</Typography>
+        </Box>
+        {props.action}
       </Box>
-      <Box minWidth={35}>
-        <Typography variant="body2" color="textSecondary">{`${Math.round(props.value,)}%`}</Typography>
-      </Box>
-      {props.action}
-    </Box>
+    </div>
   )
 }
 
